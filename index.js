3975985c6fda9f6b5c6700ffa9828f4dac46df21
moerefine <moe@refine.agency>
2023-12-06 12:36:10 +0000

    new

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 2000;
@@ -11,48 +11,56 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://testuser:pass@cluster0.a1si9rk.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+mongoose
+    .connect(
+        "mongodb+srv://testuser:pass@cluster0.a1si9rk.mongodb.net/?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
 
 // User model
-const User = mongoose.model('User', new mongoose.Schema({
-    username: String,
-    address: String,
-}));
+const User = mongoose.model(
+    "User",
+    new mongoose.Schema({
+        username: String,
+        address: String,
+        tokens: Number,
+    })
+);
 
 // Route to add a user
-app.post('/adduser', async (req, res) => {
-    const { username, address } = req.body;
-    if (!username || !address) {
-        return res.status(400).send('Username and address are required');
+app.post("/adduser", async (req, res) => {
+    const { username, address, tokens } = req.body;
+    if (!username || !address || !tokens) {
+        return res.status(400).send("Username and address are required");
     }
 
     try {
         // Check if the address already exists and update the username if it does
         const updatedUser = await User.findOneAndUpdate(
             { address }, // find a document with this address
-            { username: username.toLowerCase() }, // update the username
+            { username: username.toLowerCase(), tokens }, // update the username
             { new: true, upsert: true } // options: return the updated document and create it if it doesn't exist
         );
 
         if (updatedUser) {
-            res.status(201).send('User added or updated successfully');
+            res.status(201).send("User added or updated successfully");
         } else {
-            res.status(404).send('User not found and not added');
+            res.status(404).send("User not found and not added");
         }
     } catch (error) {
         res.status(500).send(error.message);
     }
 });
 
-app.post('/checkuser', async (req, res) => {
+app.post("/checkuser", async (req, res) => {
     const { username } = req.body;
     if (!username) {
-        return res.status(400).send('Username is required');
+        return res.status(400).send("Username is required");
     }
 
     try {
